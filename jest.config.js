88e507868afc579c15749000a1b8a24fd2e35dc4
apktntaj <maiaelarsya@gmail.com>
2026-07-17 23:02:45 +0700

    feat: add TypeScript support to Jest configuration and update dependencies

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,20 @@ const config = {
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
+    '^@core/(.*)$': '<rootDir>/core/$1',
   },
   transform: {
     '^.+\\.(js|jsx|mjs|cjs)$': ['babel-jest', {
       presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
     }],
+    '^.+\\.ts$': ['babel-jest', {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+      ],
+    }],
   },
+  moduleFileExtensions: ['ts', 'js', 'jsx', 'json'],
 };
 
 module.exports = config;
