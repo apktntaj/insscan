139c04a33f512cff8fdf8bf81e5989a079d16da2
apktntaj <maiaelarsya@gmail.com>
2026-07-17 23:03:16 +0700

    feat: add TypeScript types for Lartas and related entities

---
+++ core/cek-lartas/types.ts
@@ -0,0 +1,44 @@
+export type HsCode = {
+  readonly code: string;
+};
+
+export type KategoriLartas = "border" | "post-border" | "ekspor";
+
+export type LartasDetail = {
+  namaIzin: string;
+  kodeIzin: string | null;
+  noSkep: string | null;
+  idDokumen: string | null;
+  dokumenPabean: string[] | null;
+  tanggalMulai: string | null;
+  tanggalAkhir: string | null;
+  link: string | null;
+};
+
+export type Tarif = {
+  bm: string | null;
+  ppn: string | null;
+  pph: string | null;
+  pphNonApi: string | null;
+};
+
+export type Lartas = {
+  hsCode: HsCode;
+  tarif: Tarif;
+  regulasi: Map<KategoriLartas, LartasDetail[]>;
+};
+
+export type RawInsw = {
+  [key: string]: unknown;
+  bm?: unknown;
+  ppn?: unknown;
+  pph?: unknown;
+  pphNonApi?: unknown;
+  lartasBorderDetails?: unknown;
+  lartasPostBorderDetails?: unknown;
+  lartasExportDetails?: unknown;
+};
+
+export type Result<T> =
+  | { ok: true; data: T }
+  | { ok: false; error: string };
